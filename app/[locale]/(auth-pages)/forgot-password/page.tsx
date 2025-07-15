3a08332e48b20b90eb5644fa82c3597acb125312
EAlexBen <107051871+EB378@users.noreply.github.com>
2025-07-15 23:56:18 +0000

    modified:   app/[locale]/(auth-pages)/forgot-password/page.tsx 	new file:   components/auth-page2/index.tsx

--- app/[locale]/(auth-pages)/forgot-password/page.tsx
+++ app/[locale]/(auth-pages)/forgot-password/page.tsx
@@ -1,4 +1,4 @@
-import { AuthPage } from "@/components/auth-page";
+import { AuthPage } from "@/components/auth-page2";
 import { getData } from "@hooks/getData";
 import { redirect } from "next/navigation";
 
